621b26dfab15a7ab7755dea18b997cf6721b21c3
chineduCoded <chinedujohn17@gmail.com>
2023-07-11 10:56:09 +0000

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations 
+    needed to obtain exactly n 'H' characters in a text file.
+    
+    Args:
+        n (int): The target number of 'H' characters.
+    
+    Returns:
+        int: The minimum number of operations required. 
+        If it is impossible to achieve n 'H' characters, returns 0.
+    """
+    if n == 0:
+        return 0
+
+    dp = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        dp[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+
+    return dp[n] if dp[n] != float('inf') else 0
