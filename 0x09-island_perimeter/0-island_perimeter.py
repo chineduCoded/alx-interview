84dc98e9ddbda8abceb8ab1a23d949004ac6fcc7
chineduCoded <chinedujohn17@gmail.com>
2023-08-29 23:43:20 +0000

    Modified the code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,26 +15,30 @@ def island_perimeter(grid):
     visited = [[False] * cols for _ in range(rows)]
     perimeter = 0
 
-    def dfs(row, col):
-        if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] == 0:
-            return 1
-
-        if visited[row][col]:
-            return 0
-
-        visited[row][col] = True
-        neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        cell_perimeter = 0
-
-        for dr, dc in neighbors:
-            cell_perimeter += dfs(row + dr, col + dc)
-
-        return cell_perimeter
-
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += dfs(i, j)
+                perimeter += dfs(i, j, grid, visited)
                 break
 
     return perimeter
+
+def dfs(row, col, grid, visited):
+    """
+     Depth-First Search (DFS) function to calculate
+     the perimeter contribution of a land cell.
+     """
+    if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == 0:
+        return 1
+
+    if visited[row][col]:
+        return 0
+
+    visited[row][col] = True
+    neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    cell_perimeter = 0
+
+    for dr, dc in neighbors:
+        cell_perimeter += dfs(row + dr, col + dc, grid, visited)
+
+    return cell_perimeter
